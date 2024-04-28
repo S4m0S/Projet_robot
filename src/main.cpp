691ea631f3f5781a76c3638c4963eacc94a31748
S4m0S <adamnasreddine@outlook.com>
2024-04-28 14:24:57 +0200

    start

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
